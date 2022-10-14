c3f2be2c32fa1ecbcbc4839a0e6318b60acd6929
gabriel <gabrielcmoris@gmail.com>
2022-10-14 11:43:51 +0200

    big-o

---
+++ big-o/scalability.js
@@ -0,0 +1,18 @@
+const nemo = ["nemo"];
+const bigArr = new Array(100000).fill("nemo");
+function findNemo(arr) {
+  //check the performance
+  let t0 = performance.now();
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "nemo") {
+      console.log("FOUND NEMO!!");
+    }
+  } // O(n)
+  let t1 = performance.now();
+  console.log("The call findNemo took " + (t1 - t0) + "ms");
+}
+// console.log("1");
+// findNemo(nemo);
+// console.log("2");
+// findNemo(bigArr);
+// BIG O to measure the algorithm efficiency (scalability)
