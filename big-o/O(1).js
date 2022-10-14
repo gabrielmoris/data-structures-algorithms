8647b23bb4b1aa3cb15051d21a82be1fb8a6f3ac
gabriel <gabrielcmoris@gmail.com>
2022-10-14 13:04:07 +0200

    O(1)

---
+++ big-o/O(1).js
@@ -0,0 +1,11 @@
+function smallO(arr) {
+  return arr[0];
+}
+
+//O(1) always the same operations
+
+function smallO2(arr) {
+  console.log(arr[0]);
+  console.log(arr[1]);
+}
+//O(2) always the same operations
