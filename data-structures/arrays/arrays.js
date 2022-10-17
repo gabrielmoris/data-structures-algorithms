dd6f8f05ed72893adad71f1d3a8e3a00a8618605
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-10-17 19:45:31 +0200

    How the Array Works

--- data-structures/arrays/arrays.js
+++ data-structures/arrays/arrays.js
@@ -20,3 +20,52 @@ strings.unshift("x"); //O(n) I have to change all indexes after I put x at the b
 
 //splice
 strings.splice(2, 0, "alien"); //O(n), I also have to change indexes. Args(where I divide, how many items I delete, item added)
+
+// HOW TO BUILD AN ARRAY FROM SCRATCH
+
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+  get(index) {
+    return this.data[index]; //O(1)
+  }
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length; //O(1)
+  }
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem; //O(1)
+  }
+  delete(index) {
+    // When I call shiftItems is O(n)
+    const item = this.data[index];
+    this.shiftItems(index);
+  }
+
+  shiftItems(index) {
+    const lastItem = this.data[index];
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+
+    delete this.data[this.length - 1];
+    this.length--;
+
+    return lastItem;
+  }
+}
+
+const newArr = new MyArray();
+newArr.push("Hola");
+newArr.push("gominola");
+newArr.pop();
+newArr.push("que tal?");
+newArr.push("!");
+newArr.delete(1);
+console.log(newArr);
