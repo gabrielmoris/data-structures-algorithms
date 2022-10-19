47e8d7e790327dbe994d600de5b9cfd6c03c1cc4
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-10-19 21:51:07 +0200

    long solution added to merge and sort arrays

--- data-structures/arrays/interview-questions.js
+++ data-structures/arrays/interview-questions.js
@@ -26,7 +26,39 @@ const reverse3 = (str) => [...str].reverse().join("");
 console.log(reverse3(inputStr));
 
 // Merge sorted Arrays
-function mergeSortedArrays(arr1 = [], arr2 = []) {
+function mergeSortedArrays(array1, array2) {
+  const mergedArray = [];
+  let array1Item = array1[0];
+  let array2Item = array2[0];
+  let i = 1;
+  let j = 1;
+
+  //We should actually move these 2 if statements to line 2 so that we do the checks before we do assignments in line 3 and 4!
+  if (array1.length === 0) {
+    return array2;
+  }
+  if (array2.length === 0) {
+    return array1;
+  }
+  //O(n)
+  while (array1Item || array2Item) {
+    if (array2Item === undefined || array1Item < array2Item) {
+      mergedArray.push(array1Item);
+      array1Item = array1[i];
+      i++;
+    } else {
+      mergedArray.push(array2Item);
+      array2Item = array2[j];
+      j++;
+    }
+  }
+  return mergedArray;
+}
+
+console.log(mergeSortedArrays([0, 3, 4, 31], [3, 4, 6, 30]));
+
+//Much better
+function mergeSortedArraysBetter(arr1 = [], arr2 = []) {
   return [...arr1, ...arr2].sort(); //O(1)
 }
-console.log(mergeSortedArrays([1, 2, 3, 4, 5], [6, 5, 4, 3, 2, 1]));
+console.log(mergeSortedArraysBetter([1, 2, 3, 4, 5], [6, 5, 4, 3, 2, 1]));
