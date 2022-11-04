7760f279fbddca9ae9015276a39b7583c30eac02
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-11-04 20:41:41 +0100

    linked list explanation

---
+++ data-structures/linked-list/linked-list.js
@@ -0,0 +1,13 @@
+const basket = ["apples", "oranges", "grapes"];
+
+//linked list would be:
+
+// apples
+//     8923 --> oranges
+//                 8924 --> grapes
+//                             8925 --> null
+
+// Pointer: reference to another place in memory, object, data...
+//Example:
+const obj1 = { a: true };
+const obj2 = obj1;
