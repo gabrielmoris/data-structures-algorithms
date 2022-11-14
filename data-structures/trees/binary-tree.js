9fff43900f8bf6b13ed200df9e1bd6f0b20bbff7
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-11-14 19:39:55 +0100

    binary search tree

--- data-structures/trees/binary-tree.js
+++ data-structures/trees/binary-tree.js
@@ -1,7 +1,46 @@
-class BinaryTreeNode {
+class Node {
   constructor(value) {
-    this.value = value;
     this.left = null;
     this.right = null;
+    this.value = value;
   }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    //Code here
+    const newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+  }
+  lookup(value) {
+    //Code here
+  }
+  // remove
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+// tree.insert(4);
+// tree.insert(6);
+// tree.insert(20);
+// tree.insert(170);
+// tree.insert(15);
+// tree.insert(1);
+console.log(JSON.stringify(traverse(tree.root)));
+
+//     9
+//  4     20
+//1  6  15  170
+
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
 }
