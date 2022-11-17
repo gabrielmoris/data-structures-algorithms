c4898e898c64bd8ea1959b1982f9b2340a1d62ec
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-11-17 17:47:04 +0100

    binary-tree insert

--- data-structures/trees/binary-tree.js
+++ data-structures/trees/binary-tree.js
@@ -16,22 +16,43 @@ class BinarySearchTree {
     if (!this.root) {
       this.root = newNode;
       return this;
+    } else {
+      let currentNode = this.root;
+      //Infinite loop that stops with a return
+      while (true) {
+        if (value < currentNode.value) {
+          //left
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          //right
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
     }
   }
   lookup(value) {
     //Code here
   }
+
   // remove
 }
 
 const tree = new BinarySearchTree();
 tree.insert(9);
-// tree.insert(4);
-// tree.insert(6);
-// tree.insert(20);
-// tree.insert(170);
-// tree.insert(15);
-// tree.insert(1);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
 console.log(JSON.stringify(traverse(tree.root)));
 
 //     9
