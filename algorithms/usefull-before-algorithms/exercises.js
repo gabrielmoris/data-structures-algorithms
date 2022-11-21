c5b34cc6f263a3625ca549b12c12517615c0620a
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-11-21 22:08:27 +0100

    correct recursive fibonacci

--- algorithms/usefull-before-algorithms/exercises.js
+++ algorithms/usefull-before-algorithms/exercises.js
@@ -36,6 +36,26 @@ function findFactorialIterative(number) {
   return answer2;
 } //On
 
-console.log(findFactorialRecursive(5)); //120
-console.log(findFactorialRecursive2(5)); //120
-console.log(findFactorialIterative(5)); //120
+// console.log(findFactorialRecursive(5)); //120
+// console.log(findFactorialRecursive2(5)); //120
+// console.log(findFactorialIterative(5)); //120
+// 0 1 1 2 3 5 8 13 21
+
+function fibonacciIterative(n) {
+  let arr = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
+  }
+  return arr[n];
+}
+
+function fibonacciRecursive(n) {
+  if (n < 2) {
+    return n;
+  }
+  console.log(n);
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+}
+
+console.log(fibonacciIterative(8));
+console.log(fibonacciRecursive(8));
