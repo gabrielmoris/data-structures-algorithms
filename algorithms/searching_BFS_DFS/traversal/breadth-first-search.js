08ef53a4d04cfc0c1a3cc8783bc4da03755fc9c6
gabriel <gabrielcmoris@gmail.com>
2023-01-05 15:58:33 +0100

    BFS implementation

---
+++ algorithms/searching_BFS_DFS/traversal/breadth-first-search.js
@@ -0,0 +1,99 @@
+// Check all the nodes of the same level until I reach the last one
+
+// this is a Binary tree
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    //Code here
+    const newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    } else {
+      let currentNode = this.root;
+      //Infinite loop that stops with a return
+      while (true) {
+        if (value < currentNode.value) {
+          //left
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          //right
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  } //O logN
+
+  // this is the common method to use Breadth First Search
+  breadthFirstSearch() {
+    let currentNode = this.root;
+    let list = []; // the result list
+    let queue = []; // To track the queue
+    queue.push(currentNode);
+
+    while (queue.length > 0) {
+      currentNode = queue.shift();
+      list.push(currentNode.value);
+      if (currentNode.left) {
+        queue.push(currentNode.left);
+      }
+      if (currentNode.right) {
+        queue.push(currentNode.right);
+      }
+    }
+    return list;
+  }
+
+  // But I can implemente it recursively
+  breadthFirstSearchR(queue, list) {
+    if (queue.length === 0) {
+      return list;
+    }
+    let currentNode = queue.shift();
+    list.push(currentNode.value);
+    if (currentNode.left) {
+      queue.push(currentNode.left);
+    }
+    if (currentNode.right) {
+      queue.push(currentNode.right);
+    }
+    return this.breadthFirstSearch(queue, list);
+  }
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+console.log(tree.breadthFirstSearch());
+console.log(tree.breadthFirstSearchR([tree.root], []));
+// This is the structure of the Tree
+
+//     9
+//  4     20
+//1  6  15  170
+
+// This is the result:
+// [9, 4, 20, 1, 6, 15, 170];
