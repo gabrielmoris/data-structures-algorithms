020a06c5adab445e47c5fa1f9638f23f77326cd5
gabriel <gabrielcmoris@gmail.com>
2023-01-09 19:41:32 +0100

    memoization, clousures, dinamic programming

--- algorithms/dynamic_programming/example_dinamic_programming.js
+++ algorithms/dynamic_programming/example_dinamic_programming.js
@@ -7,7 +7,7 @@ function fibonacci(n) {
     return n;
   }
   return fibonacci(n - 1) + fibonacci(n - 2);
-}
+} // On²
 
 // This function is On² trying with 50 would be impossible for the computer
 console.log("1. Function", fibonacci(30));
@@ -15,7 +15,23 @@ console.log("1. Number of Calculations", calculations);
 
 // We solve it with memoization
 calculations = 0;
-function memoizedFibonacci() {}
+function memoizedFibonacci() {
+  let cache = {};
+  return function fi(n) {
+    calculations++;
+    if (n in cache) {
+      return cache[n];
+    } else {
+      if (n < 2) {
+        return n;
+      } else {
+        cache[n] = fi(n - 1) + fi(n - 2);
+        return cache[n];
+      }
+    }
+  };
+} // On
+
 const fibo = memoizedFibonacci();
-console.log("2. Function", fibo(30));
+console.log("2. Function", fibo(300));
 console.log("2. Number of Calculations", calculations);
